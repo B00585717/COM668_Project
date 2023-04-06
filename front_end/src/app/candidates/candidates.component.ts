ae642afd1fc2c6f6a6bbede063748978d765289b
Ruairi <coleman-r@ulster.ac.uk>
2023-04-06 11:11:48 +0100

    methods added to candidates to allow recording of votes

--- front_end/src/app/candidates/candidates.component.ts
+++ front_end/src/app/candidates/candidates.component.ts
@@ -2,6 +2,8 @@ import {Component} from "@angular/core";
 import {WebService} from "../web.service";
 import {FormBuilder} from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
+import {VoteService} from "../vote.service";
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'candidates',
@@ -14,12 +16,25 @@ export class CandidatesComponent {
   candidate_list: any = [];
   candidate_form: any;
 
+  voter_id: any;
 
-  constructor(public webService: WebService, private route: ActivatedRoute, private formBuilder: FormBuilder) {
+  constructor(public webService: WebService, private route: ActivatedRoute, private formBuilder: FormBuilder, private voteService: VoteService, private authService: AuthService) {
+    const user = this.authService.getUser();
+    this.voter_id = user ? user.voter_id : null;
+  }
+
+  onVoteButtonClick(candidate_id: number) {
+    console.log('Voter ID:', this.voter_id);
+    console.log('Candidate ID:', candidate_id);
+    this.voteService.voteForCandidate(this.voter_id, candidate_id).subscribe(
+      response => console.log('Vote submitted', response),
+      error => console.error('Error submitting vote', error)
+    );
   }
 
   ngOnInit() {
     this.candidate_list = this.webService.getCandidates();
+    this.voter_id = this.authService.getUser().voter_id;
   }
 
   onSubmit() {
@@ -35,6 +50,5 @@ export class CandidatesComponent {
       constituency_id: '',
       statement:'',
     });
-
   }
 }
