d0b13683f131ad4f3397a59bcd7310c950b13b27
Ruairi <coleman-r@ulster.ac.uk>
2023-04-06 11:10:14 +0100

    new code to access new user info when logged in

--- front_end/src/app/login/login.component.ts
+++ front_end/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 import {WebService} from "../web.service";
-import {FormBuilder, Validators} from "@angular/forms";
-import { Router } from '@angular/router';
+import {Router} from '@angular/router';
+import {AuthService} from '../auth.service';
 
 
 @Component({
@@ -13,14 +13,19 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   gov_id: any;
   password: any;
+  loggedInUserData: any;
 
-  constructor(private webService: WebService, private router: Router) {}
+  constructor(private webService: WebService, private router: Router, private authService: AuthService) {}
 
   onSubmit() {
     this.webService.login({ gov_id: this.gov_id, password: this.password }).subscribe(
-      () => {
+      (response: any) => {
         console.log('Login successful');
+        const accessToken = response.access_token;
+        const loggedInUserData = response.user_data;
         this.router.navigate(['/profile']);
+        this.authService.setUser(loggedInUserData);
+        console.log(response);
       },
       (error: any) => {
         console.error('Login failed:', error);
